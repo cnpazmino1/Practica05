461a2a5cd916c93976c8ee3d9486bc4274ca66da
Cristhian Pazmiño <cristhian170600@gmail.com>
2025-06-30 18:02:41 -0500

    implementado el logging

--- librodiario.py
+++ librodiario.py
@@ -1,5 +1,17 @@
+import logging
 from datetime import datetime
 from typing import List, Dict
+
+#cinfiguracion de loggin
+
+import logging
+
+logging.basicConfig(
+    filename="log_contable.log",
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
+)
+
 
 class MontoInvalidoError(Exception):
     """Excepción personalizada ."""
@@ -15,14 +27,19 @@ class LibroDiario:
         """Agrega una transacción al libro diario."""
         tipo = tipo.lower()
         if tipo not in ("ingreso", "egreso"):
+            mensaje = f"Tipo de transacción inválido ({tipo}). Use 'ingreso' o 'egreso'."
+            logging.error(mensaje)
             raise ValueError(f"Tipo de transacción inválido ({tipo}). Use 'ingreso' o 'egreso'.")
-        
         try:
             obj_fecha= datetime.strptime(fecha, "%d/%m/%Y")
         except Exception as e:
+            mensaje = f" Formato de fecha invalida({fecha}). use 'dd/mm/yyyy'."
+            logging.error(mensaje)
             raise ValueError(f" Formato de fecha invalida({fecha}). use 'dd/mm/yyyy'.")
 
         if monto < 0:
+            mensaje = f" monto invalido({monto}). el monto debe ser mayor a 0 ."
+            logging.error(mensaje)
             raise ValueError(f" monto invalido({monto}). el monto debe ser mayor a 0 .")
 
 
@@ -33,6 +50,7 @@ class LibroDiario:
             "tipo": tipo
         }
         self.transacciones.append(transaccion)
+        logging.info("Transaccion ({fehca} - ${monto}) exitosa")
 
     def calcular_resumen(self) -> Dict[str, float]:
         """Devuelve el resumen total de ingresos y egresos."""
